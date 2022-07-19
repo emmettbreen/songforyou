5a0e23e3a51ad1271328b8c34853ea9448a919cd
Emmett Breen <90010213+emmettbreen@users.noreply.github.com>
2022-07-19 18:44:38 -0400

    Delete forms.py

--- forms.py
+++
@@ -1,8 +0,0 @@
-from wtforms import Form, StringField, SelectField, SubmitField
-
-class SongSearchForm(Form):
-    search = StringField('')
-
-class SongSelectForm(Form):
-    select = SelectField('Search for music:', choices=[])
-    add = SubmitField()
